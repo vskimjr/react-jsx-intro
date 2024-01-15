76892fde3fcb4080e4e841b96c920d87572e40da
Nathan Rydel <nathanrydel@gmail.com>
2024-01-15 13:33:43 -0700

    fixed bug with Date not rendering

--- part-2/App.js
+++ part-2/App.js
@@ -12,19 +12,19 @@ function App() {
         name="Victor"
         username="vskimjr"
         message="Hello world! I am a Knicks fan"
-        date={new Date().toDateString}
+        date={new Date().toDateString()}
       />
       <Tweet
         name="Nate"
         username="nrdevs"
         message="Hello world! I am a Bulls fan"
-        date={new Date().toDateString}
+        date={new Date().toDateString()}
       />
       <Tweet
         name="Diane"
         username="tuckerdiane"
         message="what are sports?"
-        date={new Date().toDateString}
+        date={new Date().toDateString()}
       />
     </div>
   );
