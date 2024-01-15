ea93bafbfb8e6bd7c79020e87e3a085ab14d88b4
Nathan Rydel <nathanrydel@gmail.com>
2024-01-15 13:20:52 -0700

    added JSX to App

---
+++ part-2/App.js
@@ -0,0 +1,31 @@
+/** Main app file to render out components to app entry point
+ *
+ * Renders out user tweets
+ *
+*/
+
+
+function App() {
+  return (
+    <div>
+      <Tweet
+        name="Victor"
+        username="vskimjr"
+        message="Hello world! I am a Knicks fan"
+        date={new Date()}
+      />
+      <Tweet
+        name="Nate"
+        username="nrdevs"
+        message="Hello world! I am a Bulls fan"
+        date={new Date()}
+      />
+      <Tweet
+        name="Diane"
+        username="tuckerdiane"
+        message="what are sports?"
+        date={new Date()}
+      />
+    </div>
+  );
+}
